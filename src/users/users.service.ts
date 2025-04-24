8d440e971d7e30a374a93c24a94f2dd11d58c2cf
Faroukdev001 <faroukarogundade@gmail.com>
2025-04-24 23:31:15 +0100

    Implement UsersService for user management functionality

---
+++ src/users/users.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+
+
+// Database Layer
+@Injectable()
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private userRepo: Repository<User>,
+    ) {}
+
+    async findByEmail(email: string): Promise<User | null> {
+        return this.userRepo.findOne({ where: { email } });
+    }
+
+    async findById(id: number): Promise<User | null> {
+        return this.userRepo.findOne({ where: { id } });
+    }
+
+    async createUser(email: string, password: string): Promise<User> {
+        const user = this.userRepo.create({ email, password });
+        return this.userRepo.save(user);
+    }
+}
