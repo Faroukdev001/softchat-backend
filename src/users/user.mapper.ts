2f555262d31242d54bc665f06ac5cb08d24cc726
Faroukdev001 <faroukarogundade@gmail.com>
2025-04-24 23:49:16 +0100

    Add user mapper function to transform User entity to UserResponseDto

---
+++ src/users/user.mapper.ts
@@ -0,0 +1,7 @@
+import { User } from './user.entity';
+import { UserResponseDto } from './dto/user-response.dto';
+
+export function toUserResponseDto(user: User): UserResponseDto {
+  const { id, email, softPoints, createdAt } = user;
+  return { id, email, softPoints, createdAt };
+}
