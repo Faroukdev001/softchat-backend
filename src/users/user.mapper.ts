d6cfcb6c030710b1179a5e1efcefd07b9431245e
Faroukdev001 <faroukarogundade@gmail.com>
2025-05-01 17:25:49 +0100

    Add role and isBanned properties to UserResponseDto in user mapper

--- src/users/user.mapper.ts
+++ src/users/user.mapper.ts
@@ -2,6 +2,6 @@ import { User } from './user.entity';
 import { UserResponseDto } from './dto/user-response.dto';
 
 export function toUserResponseDto(user: User): UserResponseDto {
-  const { id, email, softPoints, createdAt } = user;
-  return { id, email, softPoints, createdAt };
+  const { id, email, softPoints, createdAt, role, isBanned } = user;
+  return { id, email, softPoints, createdAt, role, isBanned };
 }
