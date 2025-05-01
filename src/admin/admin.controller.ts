58a76ee9b7bdc76f113ad9be3568cd77adb02f47
Faroukdev001 <faroukarogundade@gmail.com>
2025-05-01 17:23:40 +0100

    Refactor AdminController to include user role management and ban/unban functionality

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,18 +1,65 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Query, UseGuards } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { Role } from 'src/auth/enums/role.enum';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { toUserResponseDto } from 'src/users/user.mapper';
+import { UserResponseDto } from 'src/users/dto/user-response.dto';
+import { BanUserDto } from 'src/users/dto/ban-user.dto';
 
-@Controller('admin')
 @UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(Role.ADMIN)
+@Controller('admin')
 export class AdminController {
-  constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService) { }
 
-  @Roles(Role.ADMIN)
+
   @Get('users')
-  getAllUsers() {
-    return this.usersService.findAll(); // You'll need to implement this
+  async getAllUsers(
+    @Query('page') page = 1,
+    @Query('limit') limit = 10,
+    @Query('search') search?: string,
+  ) {
+    return this.usersService.findAllUsersWithPagination(+page, +limit, search); 
   }
+
+  // @Patch(':id/role')
+  //   @Param('id') id: number,
+  //   @Body('role') role: Role
+  // ) {
+  //   return this.usersService.updateRole(id, role);
+  // }
+
+  @Patch(':id/role')
+  async updateUserRole(
+    @Param('id') id: number,
+    @Body('role') role: Role
+  ): Promise<UserResponseDto> {
+    const updatedUser = await this.usersService.updateRole(id, role);
+    return toUserResponseDto(updatedUser);
+  }
+
+  @Patch(':id/ban')
+  async banUser(
+    @Param('id') id: number,
+    @Body() { reason }: BanUserDto
+  ): Promise<UserResponseDto> {
+    const bannedUser = await this.usersService.banUser(id, reason ?? 'No reason provided');
+    return toUserResponseDto(bannedUser);
+  }
+
+  @Patch(':id/unban')
+  async unbanUser(@Param('id') id: number): Promise<UserResponseDto> {
+    const restoredUser = await this.usersService.unbanUser(id);
+    return toUserResponseDto(restoredUser);
+  }
+
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string): Promise<{ message: string }> {
+    await this.usersService.deleteUser(id);
+    return { message: 'User deleted successfully' };
+  }
+
 }
