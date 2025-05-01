7225ca420984a7ca6c23354dc0d76ee9560493cf
Faroukdev001 <faroukarogundade@gmail.com>
2025-05-01 06:05:46 +0100

    Add AdminModule with controller and user imports

---
+++ src/admin/admin.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AdminController } from './admin.controller';
+import { UsersModule } from 'src/users/users.module'; // Assuming you need user data
+
+@Module({
+  imports: [UsersModule], // If admin needs to fetch users
+  controllers: [AdminController],
+})
+export class AdminModule {}
