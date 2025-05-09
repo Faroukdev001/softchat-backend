2a1579c3fecf6f0a37597f8129baa13e1a9eda0f
Faroukdev001 <faroukarogundade@gmail.com>
2025-05-09 22:39:53 +0100

    Add name property to User entity and adjust imports

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,11 +1,15 @@
 import { Role } from "src/auth/enums/role.enum";
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Product } from "src/products/product.entity";
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('users')
 export class User {
 
     @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
+    name: string;
 
     @Column({ unique: true })
     email: string;
@@ -23,13 +27,16 @@ export class User {
     isBanned: boolean;
 
     @Column({ type: 'text', nullable: true })
-    reason: string | null;    
+    reason: string | null;
 
     @Column({ type: 'timestamp', nullable: true })
-    bannedAt: Date | null;    
+    bannedAt: Date | null;
 
     @Column({ type: 'enum', enum: Role, default: Role.USER })
     role: Role;
+
+    @OneToMany(() => Product, product => product.createdBy)
+    products: Product[];
 
     @Column({ nullable: true })
     refreshToken?: string;
