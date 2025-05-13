c4b544d7ad0336d20a0125c80d9defceceef7bbe
Faroukdev001 <faroukarogundade@gmail.com>
2025-05-13 15:17:07 +0100

    Refactor Post entity by reordering imports and ensuring consistent formatting

--- src/posts/posts.entity.ts
+++ src/posts/posts.entity.ts
@@ -1,32 +1,36 @@
 // src/posts/entities/post.entity.ts
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    ManyToOne,
-    CreateDateColumn,
-    UpdateDateColumn,
-  } from 'typeorm';
-  import { User } from 'src/users/user.entity';
-  
-  @Entity()
-  export class Post {
-    @PrimaryGeneratedColumn()
-    id: number;
-  
-    @Column({ type: 'text' })
-    content: string;
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
+import { User } from 'src/users/user.entity';
+import { Comment } from 'src/comments/comment.entity';
 
-    @Column({ type: 'varchar', nullable: true }) // Explicitly define the type as varchar
-    imageUrl: string | null;
-  
-    @ManyToOne(() => User, (user) => user.posts, { onDelete: 'CASCADE' })
-    author: User;
-  
-    @CreateDateColumn()
-    createdAt: Date;
-  
-    @UpdateDateColumn()
-    updatedAt: Date;
-  }
-  
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'text' })
+  content: string;
+
+  @Column({ type: 'varchar', nullable: true }) // Explicitly define the type as varchar
+  imageUrl: string | null;
+
+  @ManyToOne(() => User, (user) => user.posts, { onDelete: 'CASCADE' })
+  author: User;
+
+  @OneToMany(() => Comment, (comment) => comment.post)
+  comments: Comment[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
