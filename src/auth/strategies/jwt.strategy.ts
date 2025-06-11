b7cbb8946590e044eaea63506466e4addffca173
Faroukdev001 <faroukarogundade@gmail.com>
2025-06-11 23:42:03 +0100

    Refactor JwtStrategy to use AuthRepository for user validation and remove unused UsersService

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,14 +1,19 @@
 import { Inject, Injectable, UnauthorizedException } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
+import { InjectRepository } from "@nestjs/typeorm";
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UsersService } from "src/users/users.service";
+import { AuthRepository } from "../auth.repository";
+import { User } from "src/users/user.entity";
 
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(
-        private usersService: UsersService,
+        // private usersService: UsersService,
+        @InjectRepository(AuthRepository)
+        private authRepository: AuthRepository,
         configService: ConfigService,
     ) {
         super({
@@ -18,10 +23,21 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         });
     }
 
-    async validate(payload: { sub: number }) {
-        const user = await this.usersService.findById(payload.sub);
-        if (!user) throw new UnauthorizedException('User not found');
+    // async validate(payload: { sub: number }) {
+    //     const user = await this.authRepository.findyId(payload.sub);
+    //     if (!user) throw new UnauthorizedException('User not found');
+    //     return user;
+    // // The user object will be available in the request object as req.user
+    // }
+
+    async validate(payload: { email: string, sub: number }) {
+        const { email } = payload;
+        const user = await this.authRepository.findOneBy({ email });
+
+        if (!user) {
+            throw new UnauthorizedException();
+        }
+
         return user;
-    // The user object will be available in the request object as req.user
     }
 }
