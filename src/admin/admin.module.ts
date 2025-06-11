9a215ae16a58c4fc2efa21a20bd0d72a1f092bb3
Faroukdev001 <faroukarogundade@gmail.com>
2025-06-11 23:43:47 +0100

    Comment out AdminModule and its dependencies for future reference

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AdminController } from './admin.controller';
-import { UsersModule } from 'src/users/users.module'; // Assuming you need user data
+// import { Module } from '@nestjs/common';
+// import { AdminController } from './admin.controller';
+// import { UsersModule } from 'src/users/users.module'; // Assuming you need user data
 
-@Module({
-  imports: [UsersModule], // If admin needs to fetch users
-  controllers: [AdminController],
-})
-export class AdminModule {}
+// @Module({
+//   imports: [UsersModule], // If admin needs to fetch users
+//   controllers: [AdminController],
+// })
+// export class AdminModule {}
