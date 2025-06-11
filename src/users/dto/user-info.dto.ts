85df5fee5cb099ef90b9d670f8ebc3129d644347
Faroukdev001 <faroukarogundade@gmail.com>
2025-06-11 23:39:23 +0100

    Add UserInfoDto for user information data transfer

---
+++ src/users/dto/user-info.dto.ts
@@ -0,0 +1,14 @@
+export class UserInfoDto {
+
+    email: string;
+    username: string;
+    thumbnail: string;
+    bookMarks: number[];
+    statusMessage: string;
+    // totalPostCount: number;
+    // followerCount: number;
+    // followingCount: number;
+    softPoints: number;
+    // softPoints: number;
+    
+}
