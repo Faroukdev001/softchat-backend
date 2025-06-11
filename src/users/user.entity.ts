29a84192e604c1386e5a12f64da62977e0dc9033
Faroukdev001 <faroukarogundade@gmail.com>
2025-06-11 23:41:35 +0100

    Refactor User entity to extend BaseEntity and update username property

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -2,16 +2,16 @@ import { Role } from "src/auth/enums/role.enum";
 import { Post } from "src/posts/posts.entity";
 import { Product } from "src/products/product.entity";
 import { Comment } from "src/comments/comment.entity";
-import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, CreateDateColumn, Entity, JoinColumn, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('users')
-export class User {
+export class User extends BaseEntity{
 
     @PrimaryGeneratedColumn()
     id: number;
 
     @Column()
-    name: string;
+    username: string;
 
     @Column({ unique: true })
     email: string;
@@ -22,8 +22,40 @@ export class User {
     @Column({ default: 0 })
     softPoints: number;
 
-    @OneToMany(() => Post, (post) => post.author)
-    posts: Post[];
+    // @OneToMany(() => Post, (post) => post.author)
+    // posts: Post[];
+
+    // @OneToMany(() => Post, post => post.user, { eager: true })
+    // @JoinColumn([{ name: 'postId', referencedColumnName: 'id' }])
+    // posts: Post[];
+
+    @Column({ default: `/images/default/dafault_thumbnail.png` })
+    thumbnail: string;
+
+    @Column('int', { array: true, nullable: false })
+    bookMarks: number[];
+
+    @Column({ default: "" })
+    statusMessage: string;
+
+    // @OneToMany(() => UserChatRoomEntity, chatRoom => chatRoom.user)
+    // @JoinColumn([{ name: 'chatRoomId', referencedColumnName: 'id' }])
+    // userChatRooms: UserChatRoomEntity[];
+
+    // @OneToMany(() => MessageEntity, message => message.sender)
+    // messages: MessageEntity[];
+
+    // @OneToMany(() => Follow, follow => follow.follower)
+    // followings: Follow[];
+
+    // @OneToMany(() => Follow, follow => follow.following)
+    // followers: Follow[];
+
+    totalPostCount: number;
+    
+    followerCount: number;
+    
+    followingCount: number;
 
     @OneToMany(() => Comment, (comment) => comment.author)
     comments: Comment[];
