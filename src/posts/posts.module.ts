767a31c4fc84cdb0f338f0817de44b52d940bff9
Faroukdev001 <faroukarogundade@gmail.com>
2025-06-15 19:52:57 +0100

    Uncomment and restructure PostsModule to enable proper module functionality

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,12 +1,31 @@
-// import { Module } from '@nestjs/common';
-// import { PostsService } from './posts.service';
-// import { PostsController } from './posts.controller';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { Post } from './posts.entity';
+import { Module } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { PostsController } from './posts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './posts.entity';
+import { PostRepository } from './posts.repository';
+import { UserRepository } from 'src/users/user.repository';
+import { AuthRepository } from 'src/auth/auth.repository';
+import { MulterModule } from '@nestjs/platform-express';
+import { AuthModule } from 'src/auth/auth.module';
 
-// @Module({
-//   imports: [TypeOrmModule.forFeature([Post])],
-//   controllers: [PostsController],
-//   providers: [PostsService],
-// })
-// export class PostsModule {}
+@Module({
+    imports: [TypeOrmModule.forFeature([Post]),
+        AuthModule,
+    MulterModule.register({
+        dest: 'uploads',
+        limits: {
+            fileSize: 1024 * 1024 * 5, // 5MB
+        },
+    }),
+    ],
+    controllers: [PostsController],
+    providers: [
+        PostsService,
+        PostRepository,
+        UserRepository,
+        AuthRepository
+    ],
+    exports: [PostsService]
+})
+export class PostsModule { }
