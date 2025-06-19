7d7ca64e80a4a9a30fbe7365b72b9b6d73fcc481
Faroukdev001 <faroukarogundade@gmail.com>
2025-06-19 14:28:06 +0100

    Add FollowRepository: Implement follow management methods including create, count, and list functionalities

---
+++ src/follow/follow.repository.ts
@@ -0,0 +1,123 @@
+import { DataSource, Repository } from "typeorm";
+import { Follow } from "./follow.entity";
+import { Injectable, Logger } from "@nestjs/common";
+import { User } from "src/users/user.entity";
+import { UserListDto } from "src/users/dto/user-list.dto";
+import { UserSimpleInfoIncludingStatusMessageDto } from "src/users/dto/user-simple-info-including-status-message.dto";
+
+@Injectable()
+export class FollowRepository extends Repository<Follow> {
+    constructor(private dataSource: DataSource) {
+        super(Follow, dataSource.createEntityManager());
+    }
+
+    // private logger = new Logger('FollowRepository');
+
+    async createFollow(follower: User, following: User): Promise<void> {
+
+        const follow = this.create({
+            follower,
+            following,
+        });
+        await this.save(follow);
+    }
+
+    /// It returns the current user following the user, or not
+    async getIsFollowing(myEmail: string, userEmail: string): Promise<boolean> {
+        const result = await this.createQueryBuilder('follow')
+            .select('COUNT(*)', 'count')
+            .leftJoin('follow.following', 'following')
+            .leftJoin('follow.follower', 'user')
+            .where('following.email = :userEmail', { userEmail })
+            .andWhere('user.email = :myEmail', { myEmail })
+            .getRawOne();
+
+        const isFollowing = result.count > 0;
+        return isFollowing;
+    }
+
+    async getFollowerCount(email: string): Promise<number> {
+        return this.createQueryBuilder('follow')
+            .leftJoinAndSelect('follow.following', 'following')
+            .where('following.email = :email', { email })
+            .getCount();
+    }
+
+    async getFollowingCount(email: string): Promise<number> {
+        return this.createQueryBuilder('follow')
+            .leftJoinAndSelect('follow.follower', 'follower')
+            .where('follower.email = :email', { email })
+            .getCount();
+    }
+
+    async cancelFollowing(follower: string, following: string): Promise<void> {
+        try {
+            await this.createQueryBuilder('follow')
+                .leftJoin('follow.follower', 'follower')
+                .leftJoin('follow.following', 'following')
+                .delete()
+                .from(Follow)
+                .where('follower.email = :follower', { follower })
+                .andWhere('following.email = :following', { following })
+                .execute();
+        } catch (error) {
+            throw new Error(`Failed to unfollow: ${error.message}`);
+        }
+    }
+
+    // async cancelFollowing(follower: string, following: string): Promise<void> {
+    //     await this.createQueryBuilder('follow')
+    //         .leftJoinAndSelect('follow.follower', 'follower')
+    //         .leftJoinAndSelect('follow.following', 'following')
+    //         .where('follower.email = :follower AND following.email = :following', { follower, following })
+    //         .delete()
+    //         .execute();
+    // }
+
+    async getFollowerList(email: string, page: number, limit: number): Promise<UserListDto> {
+
+        const query = this.createQueryBuilder('follow')
+            .leftJoinAndSelect('follow.following', 'following')
+            .leftJoinAndSelect('follow.follower', 'user')
+            .where('following.email = :email', { email })
+            .skip((page - 1) * limit)
+            .take(limit);
+
+        const [follows, total] = await query.getManyAndCount();
+
+        const userList: UserSimpleInfoIncludingStatusMessageDto[] = follows.map((follow: Follow) => {
+            const userInfo: UserSimpleInfoIncludingStatusMessageDto = new UserSimpleInfoIncludingStatusMessageDto();
+            userInfo.email = follow.follower.email;
+            userInfo.username = follow.follower.username;
+            userInfo.thumbnail = follow.follower.thumbnail;
+            userInfo.statusMessage = follow.follower.statusMessage;
+            return userInfo;
+        });
+
+        return { userList, total };
+    }
+
+    async getFollowingList(email: string, page: number, limit: number): Promise<UserListDto> {
+
+        const query = this.createQueryBuilder('follow')
+            .leftJoinAndSelect('follow.follower', 'follower')
+            .leftJoinAndSelect('follow.following', 'user')
+            .where('follower.email = :email', { email })
+            .skip((page - 1) * limit)
+            .take(limit);
+
+        const [follows, total] = await query.getManyAndCount();
+
+        const userList: UserSimpleInfoIncludingStatusMessageDto[] = follows.map((follow: Follow) => {
+            const userInfo: UserSimpleInfoIncludingStatusMessageDto = new UserSimpleInfoIncludingStatusMessageDto();
+            userInfo.email = follow.following.email;
+            userInfo.username = follow.following.username;
+            userInfo.thumbnail = follow.following.thumbnail;
+            userInfo.statusMessage = follow.following.statusMessage;
+            return userInfo;
+        });
+
+        return { userList, total };
+    }
+
+}
